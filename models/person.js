89a9f0179b9b675e7ddf554dcc43e1858138e6b1
jrzumo24 <jzunigamontejo@gmail.com>
2025-12-02 23:40:22 -0600

    Actualizacion 19

--- models/person.js
+++ models/person.js
@@ -1,27 +1,32 @@
 const mongoose = require('mongoose')
 
-//actualizacion 11
-
+// ESQUEMA CORREGIDO
 const personSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3
-    },
-    important: {
-        type: String,
-        required: true
-        
-    }
-}, { collection: 'agendas' })
-
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  content: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  important: {
+    type: String,
+    required: true
+  }
 })
 
+// TRANSFORMACIÓN A JSON
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    returnedObject.name = returnedObject.content      // content → name
+    returnedObject.number = returnedObject.important  // important → number
+
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.content
+    delete returnedObject.important
+  }
+})
+
+// EXPORTAR MODELO (sin colección forzada)
 module.exports = mongoose.model('Person', personSchema)
